488d799af18cfc174048982771dbc55d1220beb8
Gleb Hmarsky <hmarsky.gleb@yandex.ru>
2022-09-20 23:02:51 +0300

    feat(deform): fill up deform method

--- Assets/Scripts/MeshDeformer.cs
+++ Assets/Scripts/MeshDeformer.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -5,5 +6,21 @@ using UnityEngine;
 [RequireComponent(typeof(MeshFilter))]
 public class MeshDeformer : MonoBehaviour
 {
+  Mesh deformingMesh;
+  Vector3[] originalVertices, displacedVertices;
+  Vector3[] vertexVelocities;
 
+  void Start()
+  {
+    deformingMesh = GetComponent<MeshFilter>().mesh;
+    originalVertices = deformingMesh.vertices;
+    displacedVertices = new Vector3[originalVertices.Length];
+    Array.Copy(originalVertices, displacedVertices, originalVertices.Length);
+
+    vertexVelocities = new Vector3[originalVertices.Length];
+    // for (int i = 0; i < originalVertices.Length; i++)
+    // {
+    //   displacedVertices[i] = originalVertices[i];
+    // }
+  }
 }
